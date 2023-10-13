141b70d4d7701bd15fad8ce3d4a2983a1ad93217
dimka90 <dimkayilrit@gmail.com>
2023-10-13 21:38:09 +0000

    fix pycodestyle error

--- models/base_model.py
+++ models/base_model.py
@@ -15,24 +15,24 @@ class BaseModel:
     other attributes that will different each class from the other
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwarg):
         """
         A function that initialise the state of an object
         """
-        if kwargs:
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            for key, value in kwargs.items():
+        if kwarg:
+            if 'created_at' in kwarg:
+                kwarg['created_at'] = datetime.strptime(kwarg['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwarg:
+                kwarg['updated_at'] = datetime.strptime(kwarg['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwarg.items():
                 if key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
-
 
     def __str__(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,19 +4,20 @@ import os
 from models.base_model import BaseModel
 """
 A module that handles the serialization  and deserialization
-of and python object, it saves the content to a json file 
+of and python object, it saves the content to a json file
 """
+
 
 class FileStorage:
     """
-    A class that handles file implementaition and 
+    A class that handles file implementaition and
     storage
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """A function that returns all the object stored in the 
+        """A function that returns all the object stored in the
         dictionary
         """
 
@@ -45,12 +46,10 @@ class FileStorage:
         Deserialize JSON file to objects.
         """
         if os.path.isfile(FileStorage.__file_path):
-        # Reading from json 
-            with open(FileStorage.__file_path, "r", encoding = "utf-8") as file:
+            # Reading from json
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 data = json.load(file)
             for key, value in data.items():
                 class_name, obj_id = key.split('.')
                 obj = eval(class_name)(**value)
                 FileStorage.__objects[key] = obj
-                #FileStorage.__objects[key] = eval(value['__class__'] (**value)) 
-
